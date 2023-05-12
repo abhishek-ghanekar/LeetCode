62305f4b81bd683d5734ea73ecb541534f79e47a
Abhishek Ghanekar <a8hiyt@gmail.com>
2023-05-12 14:51:06 +0530

    12th may 2023

--- TopicWise Questions/dynamic programming/07_ninja_training.cpp
+++ TopicWise Questions/dynamic programming/07_ninja_training.cpp
@@ -27,3 +27,59 @@ int ninjaTraining(int n, vector<vector<int>> &points)
     vector<vector<int>> memo(n,vector<int>(4,-1));
     return f(n-1,3,points,memo);   
 }
+
+
+
+//tabulation
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<vector<int>> dp(n,vector<int>(4));
+    dp[0][0] = max(points[0][1],points[0][2]);
+    dp[0][1] = max(points[0][0],points[0][2]);
+    dp[0][2] = max(points[0][0],points[0][1]);
+    dp[0][3] = max(points[0][0],max(points[0][1],points[0][2]));
+    
+    //we go bottom to top
+    // do the process for all days
+    for(int day=1;day<n;day++) {
+        for(int last=0;last<=3;last++) {
+            dp[day][last] = 0;
+            for(int task=0;task < 3;task++) {
+                if(task != last) {
+                    int point = points[day][task] + dp[day-1][task];
+                    dp[day][last] = max(dp[day][last],point);
+                }
+            }
+        }
+    }
+    return dp[n-1][3];
+}
+
+
+//space optimization solution
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+
+    //space optimized solution
+    vector<int> prev(4,0);
+    prev[0] = max(points[0][1],points[0][2]);
+    prev[1] = max(points[0][0],points[0][2]);
+    prev[2] = max(points[0][0],points[0][1]);
+    prev[3] = max(points[0][0],max(points[0][1],points[0][2]));
+    
+    for(int day=1;day<n;day++) {
+        vector<int> temp(4);
+        for(int last=0;last<=3;last++) {
+            temp[last] = 0;
+            for(int task=0;task < 3;task++) {
+                if(task != last) {
+                    int point = points[day][task] + prev[task];
+                    temp[last] = max(temp[last],point);
+                }
+            }
+        }
+        prev = temp;
+    }
+    return prev[3];
+    
+}
