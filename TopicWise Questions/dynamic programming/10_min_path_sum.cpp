3f94fa461acad494bdd14ee3d77f5f4611b6e3a6
Abhishek Ghanekar <a8hiyt@gmail.com>
2023-05-14 18:46:53 +0530

    14th may 2023

--- TopicWise Questions/dynamic programming/10_min_path_sum.cpp
+++ TopicWise Questions/dynamic programming/10_min_path_sum.cpp
@@ -17,3 +17,53 @@ int minSumPath(vector<vector<int>> &grid) {
 }
 
 //tabulation solution
+#include <bits/stdc++.h> 
+int minSumPath(vector<vector<int>> &grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+    vector<vector<int>> dp(m,vector<int>(n));
+    // dp[0][0] = grid[0][0];
+    for(int i= 0;i<m;i++) {
+        for(int j= 0 ; j<n; j++) {
+            if(i == 0 && j == 0){
+               dp[i][j] = grid[i][j] ;
+               
+            }else {
+                int left = 1e9;
+                int top = 1e9;
+                if(i > 0) top = grid[i][j] + dp[i-1][j];
+                if(j > 0) left = grid[i][j] + dp[i][j-1];
+        
+                dp[i][j] = min(left,top);
+            } 
+            
+        }
+    }
+    return dp[m-1][n-1];
+}
+
+// Space optimization solution
+
+
+#include <bits/stdc++.h> 
+int minSumPath(vector<vector<int>> &grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+    vector<int> prev(n,0);
+    for(int i=0;i<m;i++) {
+        vector<int> curr(n,0);
+        for(int j=0;j<n;j++) {
+           if(i == 0 && j == 0) {
+                curr[j] = grid[i][j];
+            }else {
+                int top = 1e9;
+                int left = 1e9;
+                if(i > 0) top = grid[i][j] + prev[j];
+                if(j > 0) left = grid[i][j] + curr[j-1];
+                curr[j] = min(top,left);
+            }
+        }
+        prev = curr;
+    }
+    return prev[n-1];
+}
